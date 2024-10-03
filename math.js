07e1e3a78899c2e6e838552b58523d8d25cc8683
Rezamurshed <rezamurshed@gmail.com>
2024-10-03 16:57:41 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,52 @@
+console.log ("Answer the Question No 1")
+let sara = 25;
+let sarabuy = 15;
+let saratotal = (sara + sarabuy);
+console.log ("sara have total Apple : " + ' ' + saratotal);
+
+console.log ("Answer the Question No 2")
+let Jon = 500;
+let JonSpent = 200;
+let JonInhand = (Jon - JonSpent);
+console.log ("Jon inhand : " + JonInhand + ' ' + "Taka");
+
+console.log ("Answer the Question No 3")
+let IndustryMadePerHr = 120;
+let TotalHour = 8;
+let TotalMachine = (IndustryMadePerHr * TotalHour);
+console.log ("Indutry will made : " + TotalMachine + ' ' + "Machines");
+
+console.log ("Answer the Question No 4")
+let TreeInOneRow = 12;
+let TotalRow = 8;
+let ExtraTree = 20;
+let TotalTree = (TreeInOneRow * TotalRow + ExtraTree);
+console.log ("Total Tree in the garden will have : " + TotalTree);
+
+console.log ("Answer the Question No 5")
+let RahimBox = 5;
+let OrangePerBox = 10;
+let FrindGift = 12;
+let TotalOrange = (RahimBox * OrangePerBox - FrindGift);
+console.log ("Rahim have : " + TotalOrange +' '+ "Oranges");
+
+console.log ("Answer the Question No 6")
+let TrainGo = 150;
+let Time = 3;
+let TrainGoPerHour = (TrainGo / Time);
+console.log ("Train Go Per Hour : " + ' ' + TrainGoPerHour + ' ' + "Kilometers" );
+
+console.log ("Answer the Question No 7")
+let MachineNo = 6;
+let ProductProduce = 200;
+let PerMachineProduction = (ProductProduce / MachineNo);
+let TotalMachines = (6+2);
+let TotalProduction = (PerMachineProduction * TotalMachines);
+let RestOfProduct = (TotalProduction-3);
+console.log ("Rest of Product: " + RestOfProduct);
+
+console.log ("Answer the Question No 8")
+let ChocolateNo = 120;
+let PacketNo = 9;
+let RestOfChocolate = (ChocolateNo % PacketNo);
+console.log ("Rest of Chocolate: " + RestOfChocolate);

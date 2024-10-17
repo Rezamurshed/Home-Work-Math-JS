88cb9528548b3fdd9cb92e1a2345b54127c67efc
Rezamurshed <146822839+Rezamurshed@users.noreply.github.com>
2024-10-17 21:43:48 +0600

    Add files via upload

---
+++ HWthree.js
@@ -0,0 +1,75 @@
+
+console.log("Ans the Question No 1");
+
+for (let i = 1; i <= 50; i++){
+    console.log(i);
+}
+
+console.log("Ans the Question No 2");
+
+for (let i = 33; i >= 1; i--){
+    console.log(i);
+   
+}
+
+console.log("Ans the Question No 3");
+
+let sum1 = 0;
+
+for (let i = 1; i <= 45; i++){
+    if (i % 2 === 0) {
+        sum1=sum1+i;
+    }
+}
+console.log(sum1);
+
+console.log("Ans the Question No 4");
+
+let sum2 = 0;
+
+
+for (let i = 1; i <= 10; i++){
+    if (i % 2 === 1) {
+        sum2 = sum2 + i;
+                
+    }
+    
+}
+
+console.log(Math.pow(sum2, 2));
+
+console.log("Ans the Question No 5");
+
+let ntime = 7;
+let sum3 = 0;
+
+
+for (let i = 1; i <= 10; i++){
+
+    console.log (ntime, "", "X","", i, "", "=", "", sum3 = ntime * i);
+    
+}
+
+console.log("Ans the Question No 6");
+
+let num = [1, 3, 9, 5, 8, 6, 8, 8, 3];
+console.log(num.length);
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    
+        
+
+
+
+

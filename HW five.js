0e2da11f296c626e29092e501d7a580857370d7a
Rezamurshed <146822839+Rezamurshed@users.noreply.github.com>
2024-10-30 10:02:42 +0600

    Update HW five.js

--- HW five.js
+++ HW five.js
@@ -1,6 +1,6 @@
 // Write a function called addNumbers that takes two numbers as parameters and returns their sum.
 
-console.log ("Answer the Q 1")
+console.log ("Answer to the Q 1")
  
 function addNumbers(p1, p2) {
     console.log(p1 + p2);
@@ -18,7 +18,7 @@ addNumbers(20,30);
 console.log (total);
 
 // Write a function called sumUpToN that takes a number n and returns the sum of all numbers from 1 to n. Use a loop.
-console.log ("Answer the Q 2")
+console.log ("Answer to the Q 2")
  
 
 function sumUpToN(n) {
@@ -45,7 +45,7 @@ console.log(total);
 
 
 // Write a function called sumArray that takes an array of numbers as a parameter and returns the sum of all the numbers in the array
-console.log("Answer the Q 3")
+console.log("Answer to the Q 3")
 
 let n = [1, 2, 3, 4, 5];
 
@@ -63,7 +63,7 @@ console.log(sumArray(n));
 
 // Write a function called countEvenNumbers that takes an array of numbers as a parameter and returns the count of even numbers in the array.
 
-console.log("Answer the Q 4")
+console.log("Answer to the Q 4")
 
 let x = [1, 2, 3, 4, 5, 6, 7, 8 ];
 
@@ -82,7 +82,7 @@ countEvenNumbers(x);
 
 
 // Write a function called stringLength that takes a string as a parameter and returns the length of the string.
-console.log("Answer the Q 5")
+console.log("Answer to the Q 5")
 
 let st = [1, 2, 3, 4, 5, 6, 7, 8];
 function stringLength(st) {
